604c89b0188b0301daa4805381df2f4ef7c3c354
Ronell Salunke <ronell1292k@gmail.com>
2021-01-28 22:10:30 +0530

    MarsApiService: In the retrofit object, change the ConverterFactory to a MoshiConverterFactory with our moshi Object

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -21,7 +21,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
@@ -31,7 +31,7 @@ private val moshi = Moshi.Builder()
         .build()
 
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
 
