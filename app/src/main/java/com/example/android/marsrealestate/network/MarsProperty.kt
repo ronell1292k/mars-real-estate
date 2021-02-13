018530254670bf519744beb16d82d21e988cdc6a
Ronell Salunke <ronell1292k@gmail.com>
2021-02-13 21:33:36 +0530

    MarsProperty: Brackets are a pain
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -26,8 +26,8 @@ data class MarsProperty(
         val id: String,
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
-        val price: Double: Parcelable {
+        val price: Double) : Parcelable {
 
 }
-)
+
 
