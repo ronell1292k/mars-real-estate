18c58b963f95bfc77089334d5bfacc66235af79b
Ronell Salunke <ronell1292k@gmail.com>
2021-02-13 21:38:15 +0530

    OverviewFragment: Add an observer on navigateToSelectedProperty that calls navigate() to go to the detail screen when the MarsProperty is not null, and of course, we can't forget to call displayPropertyDetailsComplete() when navigation is done
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -20,7 +20,9 @@ package com.example.android.marsrealestate.overview
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
 import com.example.android.marsrealestate.databinding.GridViewItemBinding
@@ -44,6 +46,14 @@ class OverviewFragment : Fragment() {
         // Initialize PhotoGridAdapter
         binding.photosGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
             viewModel.displayPropertyDetails(it)
+        })
+
+        viewModel.navigateToSelectedProperty.observe(viewLifecycleOwner, Observer {
+            if (null != it) {
+                this.findNavController().navigate(OverviewFragmentDirections.actionShowDetail(it))
+                viewModel.displayPropertiesComplete()
+            }
+
         })
 
         setHasOptionsMenu(true)
