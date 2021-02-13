94efe98983b217b486805cde759477c3c3edd865
Ronell Salunke <ronell1292k@gmail.com>
2021-02-13 22:01:16 +0530

    DetailViewModel: Add import for resources, why didn't this get automatically added, smh
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -19,6 +19,7 @@ package com.example.android.marsrealestate.detail
 import android.app.Application
 import androidx.lifecycle.*
 import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.R
 
 /**
  * The [ViewModel] that is associated with the [DetailFragment].
