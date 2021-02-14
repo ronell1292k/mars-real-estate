cc6a43e9fd2eea265cd977741d2cae9db79b2931
Ronell Salunke <ronell1292k@gmail.com>
2021-02-14 22:49:33 +0530

    MarsApiService: Add a @Query("filter") parameter to getProperties() so we can filter properties based on the MarsApiFilter enum values
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -25,6 +25,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
@@ -42,7 +43,7 @@ private val retrofit = Retrofit.Builder()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties():
+    fun getProperties(@Query("filter") type: String):
             Deferred<List<MarsProperty>>
 }
 
