a2bc76dbe13c6981b37930474c529c9806f4f15b
Ronell Salunke <ronell1292k@gmail.com>
2022-01-10 17:18:34 +0530

    chore: replace kotlin-android-extensions with kotlin-parcelize implementation of parcelization

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -19,7 +19,7 @@ package com.example.android.marsrealestate.network
 
 import android.os.Parcelable
 import com.squareup.moshi.Json
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class MarsProperty(
